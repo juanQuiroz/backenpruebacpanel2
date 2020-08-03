515e24d29bead235d946171688d6884946885003
JuanQuiroz <jaqp_911@hotmail.com>
2020-08-03 15:02:16 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
     "https://mdph-enlinea.herokuapp.com/",
   );
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
   res.header(
     "Access-Control-Allow-Headers",
     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method",
